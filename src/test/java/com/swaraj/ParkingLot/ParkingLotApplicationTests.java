f0dfe5c3b06b47599162f81f672c2e74af6f591f
SWARAJ836 <4swaraj.life4@gmail.com>
2025-06-25 12:54:44 +0530

    Initial commit

---
+++ src/test/java/com/swaraj/ParkingLot/ParkingLotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swaraj.ParkingLot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
